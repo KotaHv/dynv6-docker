e2c903feb13c64c79ad95aeaa8e6b15a05e5b427
KotaHv <92137267+KotaHv@users.noreply.github.com>
2022-06-12 14:59:40 +0800

    test: del set_env

--- test_dynv6.py
+++ test_dynv6.py
@@ -1,31 +1,17 @@
+import os
 import ipaddress
 
-import pytest
+os.environ["dynv6_hostname"] = "example.com"
+os.environ["dynv6_token"] = "example"
 
-from main import Dynv6, ipv4_file, ipv6_file
-
-
-def set_env(monkeypatch):
-    monkeypatch.setenv("dynv6_hostname", "example.com")
-    monkeypatch.setenv("dynv6_token", "example")
+from src.dynv6.dynv6 import Dynv6, ipv4_file, ipv6_file
 
 
-def test_init(monkeypatch):
-    with pytest.raises(KeyError, match=r".*dynv6_hostname.*"):
-        Dynv6()
-    monkeypatch.setenv("dynv6_hostname", "example.com")
-    with pytest.raises(KeyError, match=r".*dynv6_token.*"):
-        Dynv6()
-    monkeypatch.delenv("dynv6_hostname")
-    monkeypatch.setenv("dynv6_token", "example")
-    with pytest.raises(KeyError, match=r".*dynv6_hostname.*"):
-        Dynv6()
-    set_env(monkeypatch)
+def test_init():
     Dynv6()
 
 
-def test_load_ip(monkeypatch):
-    set_env(monkeypatch)
+def test_load_ip():
     ipv4_addr = "127.0.0.1"
     ipv6_addr = "::"
     with open(ipv6_file, "w") as f:
@@ -39,8 +25,7 @@ def test_load_ip(monkeypatch):
     assert dynv6.ipv6_addr == ipv6_addr
 
 
-def test_change_ip(monkeypatch):
-    set_env(monkeypatch)
+def test_change_ip():
     ipv4_addr = "127.0.1.1"
     ipv6_addr = "::1"
     dynv6 = Dynv6()
@@ -54,8 +39,7 @@ def test_change_ip(monkeypatch):
         assert f.read() == ipv4_addr
 
 
-def test_get_ip(monkeypatch):
-    set_env(monkeypatch)
+def test_get_ip():
     dynv6 = Dynv6()
     ipv4_addr = dynv6.get_ipv4()
     assert isinstance(ipaddress.ip_address(ipv4_addr), ipaddress.IPv4Address)
